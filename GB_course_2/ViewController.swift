74a5f2f4ec7969b7b023ac1b093e5bc9bbfd418c
MaxLi92 <m.li.92@icloud.com>
2021-10-07 22:22:20 +0600

    Initial Commit

---
+++ GB_course_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GB_course_2
+//
+//  Created by Max Li on 07.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
